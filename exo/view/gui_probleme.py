8daa4d6ed6fd07285a326e56ffaab3b80858c5a1
dev <dev@dev.dev>
2022-11-16 15:21:03 +0100

    passage de notre GUI en class

--- exo/view/gui_probleme.py
+++ exo/view/gui_probleme.py
@@ -1,47 +1,40 @@
 from tkinter import *
 
+
 class Gui_probleme:
     def __init__(self, parent):
         self.parent = parent
+        self.parent.geometry("200x200")
         self.frame = Frame(parent)
+        self.interfaceGUI()
+        self.packGUI()
+
+    def interfaceGUI(self):
+        self.labNbEleves = Label(self.frame, text="Nombre d'élèves")
+        self.enNbEleves = Entry(self.frame)
+        self.labNbAdultes = Label(self.frame, text="Nombre d'adultes")
+        self.enNbAdultes = Entry(self.frame)
+        self.labNbPlacesCars = Label(self.frame, text="Nombre de places par cars")
+        self.enNbPlacesCars = Entry(self.frame)
+        self.retourBtn = Button(self.frame, text="Calcul", command=self.calcul)
+        self.labNbCars = Label(self.frame, text="Nombre de cars")
+
+    def packGUI(self):
         self.frame.pack()
-        self.button = Button(self.frame, text="QUIT", fg="red", command=self.frame.quit)
-        self.button.pack(side=LEFT)
-        self.slogan = Button(self.frame, text="Hello", command=self.write_slogan)
-        self.slogan.pack(side=LEFT)
+        self.labNbEleves.pack()
+        self.enNbEleves.pack()
+        self.labNbAdultes.pack()
+        self.enNbAdultes.pack()
+        self.labNbPlacesCars.pack()
+        self.enNbPlacesCars.pack()
+        self.retourBtn.pack()
+        self.labNbCars.pack()
 
-    def write_slogan(self):
-        print("Tkinter is easy to use!")
-
-# fen = Tk()
-# fen.geometry("200x200")
-# fen.title("Nombre de cars et de places vides")
-
-# def calcul():
-#     try:
-#         nb_eleve = int(enNbEleves.get())
-#         nb_adulte = int(enNbAdultes.get())
-#         nb_place = int(enNbPlacesCars.get())
-#         labNbCars.config(text="test", fg="green")
-#     except:
-#         labNbCars.config(text="Erreur de saisie", fg="red")
-
-# labNbEleves = Label(fen, text="Nombre d'élèves")
-# enNbEleves = Entry(fen)
-# labNbAdultes = Label(fen, text="Nombre d'adultes")
-# enNbAdultes = Entry(fen)
-# labNbPlacesCars = Label(fen, text="Nombre de places par cars")
-# enNbPlacesCars = Entry(fen)
-# retourBtn = Button(fen, text="Calcul", command=calcul)
-# labNbCars = Label(fen, text="Nombre de cars")
-
-# labNbEleves.pack()
-# enNbEleves.pack()
-# labNbAdultes.pack()
-# enNbAdultes.pack()
-# labNbPlacesCars.pack()
-# enNbPlacesCars.pack()
-# retourBtn.pack()
-# labNbCars.pack()
-
-# fen.mainloop()
+    def calcul(self):
+        try:
+            nb_eleve = int(self.enNbEleves.get())
+            nb_adulte = int(self.enNbAdultes.get())
+            nb_place = int(self.enNbPlacesCars.get())
+            self.labNbCars.config(text="test", fg="green")
+        except:
+            self.labNbCars.config(text="Erreur de saisie", fg="red")
